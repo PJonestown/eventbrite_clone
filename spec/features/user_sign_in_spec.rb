8665d9c15f00005bd859612c081457ca45297392
Newton <anthpaul0@gmail.com>
2015-07-08 13:59:43 -0500

    sign in/out integration test'

---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'user sign in/out proccess' do
+  context 'registered user' do
+    it 'should sign in/out the user' do
+      #Sign in
+      user = create(:user)
+      visit root_path
+      click_link 'Sign in'
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content(user.username)
+
+      #Sign out
+      click_link 'Sign out'
+      expect(current_path).to eq(root_path)
+      expect(page).to have_link 'Sign in'
+    end
+  end
+end
