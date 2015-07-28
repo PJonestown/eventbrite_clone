db7eebd04be45a81fefada8392f7fead95200d27
Newton <anthpaul0@gmail.com>
2015-07-28 14:32:08 -0500

    Update seeds file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727224614) do
+ActiveRecord::Schema.define(version: 20150728192151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,11 +64,12 @@ ActiveRecord::Schema.define(version: 20150727224614) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "owner_id"
     t.text     "description"
     t.integer  "category_id"
+    t.boolean  "private",     default: false
   end
 
   add_index "groups", ["category_id"], name: "index_groups_on_category_id", using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,16 +14,16 @@ Category.create(name: 'Other')
 
 Group.create(name: 'Walks on the beach',
              description: 'We meet regularly every evening as if drawn by some need.',
-             owner_id: 1, category_id: 6)
+             owner_id: 1, category_id: 6, private: false)
 Group.create(name: 'Conspiracy club',
              description: 'Anything from bombs to underground post offices. You hide they seek.',
-             owner_id: 3, category_id: 6)
+             owner_id: 3, category_id: 6, private: true)
 Group.create(name: 'Mountain climbing meetup',
              description: 'The only real sport (except for racing and buullfighting of course)',
-             owner_id: 6, category_id: 4)
+             owner_id: 6, category_id: 4, private: false)
 Group.create(name: 'Tennis club',
              description: "Mandatory lemon pledge sunscreen. It’s Marlon Brando’s fault, Jim",
-             owner_id: 2, category_id: 4)
+             owner_id: 2, category_id: 4, private: false)
 
 Membership.create(member_id: 6, group_membership_id: 1)
 Membership.create(member_id: 1, group_membership_id: 1)
@@ -38,16 +38,16 @@ Membership.create(member_id: 3, group_membership_id: 4)
 Membership.create(member_id: 6, group_membership_id: 4)
 Membership.create(member_id: 2, group_membership_id: 4)
 
-Gathering.create( creator_id: 1, group_id: 1,
-                 name: 'Waves appreciation', date: Date.today + 1.year ,
+Gathering.create(creator_id: 1, group_id: 1,
+                 name: 'Waves appreciation', date: Time.zone.today + 1.year,
                  description: 'words')
-Gathering.create( creator_id: 1, group_id: 1,
-                 name: 'Sail to the lighthouse', date: Date.today - 1.year ,
+Gathering.create(creator_id: 1, group_id: 1,
+                 name: 'Sail to the lighthouse', date: Time.zone.today - 1.year,
                  description: 'more words')
-Gathering.create( creator_id: 1, group_id: 1,
-                 name: 'Painting lessons', date: Date.today + 1.month ,
+Gathering.create(creator_id: 1, group_id: 1,
+                 name: 'Painting lessons', date: Time.zone.today + 1.month,
                  description: 'He shivered; he quivered. All his vanity, all
-                               his satisfaction in his own splendour, riding 
+                               his satisfaction in his own splendour, riding
                                fell as a thunderbolt.')
 
 GatheringAttendance.create(attendee_id: 2, attended_gathering_id: 2)
@@ -57,14 +57,14 @@ GatheringAttendance.create(attendee_id: 6, attended_gathering_id: 3)
 GatheringAttendance.create(attendee_id: 1, attended_gathering_id: 3)
 GatheringAttendance.create(attendee_id: 4, attended_gathering_id: 3)
 
-Moderation.create(moderator_id: 2,moderated_group_id: 1)
-Moderation.create(moderator_id: 6,moderated_group_id: 1)
+Moderation.create(moderator_id: 2, moderated_group_id: 1)
+Moderation.create(moderator_id: 6, moderated_group_id: 1)
 
 Event.new(title: 'One time only event',
-          description: "Did I mention it's only one time and doesn't need a group?", 
-          date: Date.today + 1.month,
+          description: "Did I mention it's only one time and doesn't need a group?",
+          date: Time.zone.today + 1.month,
           creator_id: 6)
 
-Attendance.create( attendee_id: 6, attended_event_id: 1 )
-Attendance.create( attendee_id: 2, attended_event_id: 1 )
-Attendance.create( attendee_id: 4, attended_event_id: 1 )
+Attendance.create(attendee_id: 6, attended_event_id: 1)
+Attendance.create(attendee_id: 2, attended_event_id: 1)
+Attendance.create(attendee_id: 4, attended_event_id: 1)
