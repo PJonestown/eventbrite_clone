6f71e0afcef20a0274a3f2e8545e97cfc16c5ac3
Newton <anthpaul0@gmail.com>
2015-07-30 15:09:37 -0500

    Radio buttons to groups#new and update group_creation_spec

--- spec/features/group_creation_spec.rb
+++ spec/features/group_creation_spec.rb
@@ -21,6 +21,7 @@ feature 'group creation' do
       fill_in 'Name', with: 'A brand new group'
       fill_in 'Description', with: 'Now even newer!'
       select(category.name)
+      choose('Allow any member to create a gathering')
       click_button 'Start Group'
       expect(current_path).to eq group_path(user.owned_groups.last)
     end
