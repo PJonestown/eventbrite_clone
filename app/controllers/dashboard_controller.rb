5d6fee2a41bef1460c76d852abdac5e820b9a7df
Newton <anthpaul0@gmail.com>
2015-09-02 18:20:27 -0500

    Combined variable for dashboard#show

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
   def show
+    # Placeholder. Need it sorted by date of event not creation
+    @combined = (Event.all + Gathering.all).sort{ |a,b| a.created_at <=> b.created_at }
   end
 end
