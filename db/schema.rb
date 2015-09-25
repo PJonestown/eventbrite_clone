f879c09e4455d8b5434b7e02936a7e64e984d7fb
Newton <anthpaul0@gmail.com>
2015-09-25 17:53:11 -0500

    Remove address from gatherings. Closes #59

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925224854) do
+ActiveRecord::Schema.define(version: 20150925225106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,9 +71,6 @@ ActiveRecord::Schema.define(version: 20150925224854) do
     t.integer  "creator_id"
     t.integer  "group_id"
     t.boolean  "approved",    default: true
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
   end
 
   add_index "gatherings", ["creator_id"], name: "index_gatherings_on_creator_id", using: :btree
