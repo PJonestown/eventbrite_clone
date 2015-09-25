50bbfe0978ea83d32aaa7a2476b906deb7beebe7
Newton <anthpaul0@gmail.com>
2015-09-25 17:55:24 -0500

    Remove address column and others from events. Closes #58

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925225106) do
+ActiveRecord::Schema.define(version: 20150925225347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,9 +48,6 @@ ActiveRecord::Schema.define(version: 20150925225106) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "creator_id"
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
   end
 
   add_index "events", ["creator_id"], name: "index_events_on_creator_id", using: :btree
