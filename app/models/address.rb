0bf0a7a81b01597d21101994d0a3d4b815524d63
Newton <anthpaul0@gmail.com>
2015-10-28 14:49:22 -0500

    Uncomment geocoding

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,13 +2,13 @@ class Address < ActiveRecord::Base
   belongs_to :addressable, :polymorphic => true
 
   validates :location, :latitude, :longitude, :presence => true
-=begin
+
   geocoded_by :location
   before_validation :geocode,
     :if => ->(obj) { obj.location_changed? }
 
   acts_as_geolocated
-=end
+
   def self.psql_radius_search(radius, lat, long)
     within_radius(radius, lat, long)
   end
